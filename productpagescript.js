ce1424fd129f2b316ef9c0204dae1c011141d1e1
Ranu Ranjan <ranuranjan25@gmail.com>
2021-08-17 14:01:40 +0530

    All files have been arrangned in manner

--- productpagescript.js
+++
@@ -1,9 +0,0 @@
-
-// this function  will redirect to the signup page 
-function redirectToSignUp(){
-    window.location.href = "" // pest the signup page link here(Ranu your page link add here)
-}
-// this function will redirect to the landing page
-function redirectToTheLandingPage(){
-    window.location.href = "" // link of home page pest here (Ankit add the home page link here)
-}
